091b48585c6cbf12a07ae875964cbadbc5f7400f
cuong <nvcuong1982@yahoo.com>
2016-11-03 14:12:03 +0100

    adding registration

--- app/src/main/java/barcode/barclays/com/drone/DroneActivity.java
+++ app/src/main/java/barcode/barclays/com/drone/DroneActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import dji.thirdparty.eventbus.EventBus;
 
 public class DroneActivity extends AppCompatActivity {
 
@@ -15,10 +16,12 @@ public class DroneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drone);
         mUnbinder = ButterKnife.bind(this);
+        EventBus.getDefault().register(this);
     }
 
     @Override
     protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
         mUnbinder.unbind();
         super.onDestroy();
     }
