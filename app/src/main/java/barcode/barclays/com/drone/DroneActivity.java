0fc2742f06643a5e348c749f821421455107f081
Jozef Lang <developer@jozeflang.com>
2016-11-03 13:12:00 +0100

    First version of UI. Making activity toolbarless

--- app/src/main/java/barcode/barclays/com/drone/DroneActivity.java
+++ app/src/main/java/barcode/barclays/com/drone/DroneActivity.java
@@ -3,11 +3,23 @@ package barcode.barclays.com.drone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class DroneActivity extends AppCompatActivity {
+
+    private Unbinder mUnbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drone);
+        mUnbinder = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mUnbinder.unbind();
+        super.onDestroy();
     }
 }
