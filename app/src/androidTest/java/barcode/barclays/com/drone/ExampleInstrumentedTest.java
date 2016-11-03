ffc92cef397ff07896167aa9a0611211831a389c
vakosar <vackosar>
2016-11-03 17:58:51 +0100

    Impl image treshhold.

--- app/src/androidTest/java/barcode/barclays/com/drone/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/barcode/barclays/com/drone/ExampleInstrumentedTest.java
@@ -19,12 +19,16 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    public static final int SCALE = 3;
+    private static final int SCALE = 3;
+    public static final int THRESHHOLD = 3000;
+
 
     @Test
     public void parseJpeg() throws Exception {
         final Bitmap source = BitmapFactory.decodeFile("app/fotos/IMG_20161103_145320192.jpg");
         final Bitmap blured = blur(source);
+        final Bitmap thresholded = threshhold(blured);
+        thresholded.sa
     }
 
     private Bitmap blur(Bitmap source) {
@@ -32,5 +36,25 @@ public class ExampleInstrumentedTest {
         return Bitmap.createScaledBitmap(small, source.getWidth(), source.getHeight(), true);
     }
 
-//    private
+    private Bitmap treshhold(Bitmap input){
+        final Bitmap output = input.copy(input.getConfig(), false);
+        for (int x=0; x < input.getWidth(); x++) {
+            for (int y=0; y < input.getHeight(); y++) {
+                if(getBrightness(input.getPixel(x,y)) > THRESHHOLD) {
+                    output.setPixel(x, y, Color.WHITE);
+                } else {
+                    output.setPixel(x, y, Color.BLACK);
+                }
+            }
+        }
+        return output;
+    }
+
+    private int getBrightness(int color) {
+        int r = (color >> 16) & 0xFF;
+        int g = (color >> 8) & 0xFF;
+        int b = (color >> 0) & 0xFF;
+        return  (int) Math.sqrt(r * r * .241 + g * g * .691 + b * b * .068);
+    }
+
 }
